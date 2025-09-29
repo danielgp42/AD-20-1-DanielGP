fe19e4032ff18da84f1abe11421ca57b392ee13b
danielgp <danielgarduno282@gmail.com>
2025-09-29 00:29:18 -0600

    Feat: se resolvieron las tareas 1 y 3

--- js/calculator/calculator.test.js
+++ js/calculator/calculator.test.js
@@ -15,3 +15,31 @@ test('subtract 5 - 4 to equal 1', () => {
 test('subtract -5 - -10 to equal 5', () => {
   expect(calculator.subtract(-5, -10)).toBe(5);
 });
+
+test('divides 10 by 2 to equal 5', () => {
+    expect(calculator.divide(10, 2)).toBe(5);
+});
+
+test('divides 7 by 2 to equal 3.5', () => {
+    expect(calculator.divide(7, 2)).toBe(3.5);
+});
+
+test('multiplies 4 by 5 to equal 20', () => {
+    expect(calculator.multiply(4, 5)).toBe(20);
+});
+
+test('multiplies -3 by 6 to equal -18', () => {
+    expect(calculator.multiply(-3, 6)).toBe(-18);
+});
+
+test('multiplies 4 by 5 to equal 20', () => {
+    expect(calculator.multiply(4, 5)).toBe(20);
+});
+
+test('multiplies -3 by 6 to equal -18', () => {
+    expect(calculator.multiply(-3, 6)).toBe(-18);
+});
+
+test('divide by 0 should throw an error', () => {
+    expect(() => calculator.divide(10, 0)).toThrow('No se puede dividir por cero.');
+});
